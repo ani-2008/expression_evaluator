56c86fa4dcf91d7d226135c797f3bbffe8e754bd
bitabyte <anirudan2008@gmail.com>
2025-10-26 16:08:53 +0530

    fix bug division by zero

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import sys
+def rpn():
+    expr = input("")
+    expr_list = expr.split()
+    number_stack = []
+    operators = ["+","-","*","/","**","//","%"]
+    for i in expr_list:
+        if i.isdigit():
+            number_stack.append(i)
+        elif i in operators:
+            if len(number_stack) < 2:
+                print("INVALID SYNTAX")
+                
+            number1 = number_stack.pop()
+            number2 = number_stack.pop()
+            try:
+                number_stack.append(str(eval(number2+i+number1)))
+            except ZeroDivisionError as e:
+                print(e)
+                number_stack = []
+        elif i.isalpha() or i in "<>;:!@#$&_=~`\"\'()[]{}":
+            print(f"INVALID SYNTAX {i}")
+            continue            
+        else:
+            continue
 
-expr = input("EXPRESSION: ")
-expr_list = expr.split()
-number_stack = []
-operators = ["+","-","*","/","**","//","%"]
-for i in expr_list:
-    if i.isdigit():
-        number_stack.append(i)
-    elif i in operators:
-        if len(number_stack) < 2:
-            print("INVALID SYNTAX")
-            sys.exit()
-        number1 = number_stack.pop()
-        number2 = number_stack.pop()
-        number_stack.append(str(eval(number2+i+number1)))
-    elif i.isalpha() or i in "<>;:!@#$&_=~`":
-        print(f"INVALID SYNTAX {i}")
-        sys.exit()
+    if len(number_stack) == 1:
+        print(int(str(number_stack).strip("[\']")))
+    elif not len(number_stack):
+        pass
     else:
-        continue
+        print("INVALID SYNTAX: leftover operands")
+        
+def main():
+    while True:
+        print(">>>",end = " ")
+        rpn()
 
-if len(number_stack) == 1:
-    print(int(str(number_stack).strip("[\']")))
-else:
-    print("INVALID SYNTAX: leftover operands")
+main()
